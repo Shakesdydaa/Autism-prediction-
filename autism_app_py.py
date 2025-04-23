57a8f9785b63b2a8e1477105492492a92fdcb1a9
Shakesdydaa <143748152+Shakesdydaa@users.noreply.github.com>
2025-04-23 20:13:07 +0300

    Update autism_app_py.py

--- autism_app_py.py
+++ autism_app_py.py
@@ -26,20 +26,35 @@ jaundice = st.selectbox("Jaundice at birth", ['yes', 'no'])
 austim = st.selectbox("Family History of Autism", ['yes', 'no'])
 used_app_before = st.selectbox("Used App Before?", ['yes', 'no'])
 
-# Map inputs
+# Additional inputs
+ethnicity = st.selectbox("Ethnicity", ['White-European', 'Latino', 'Others'])  # Add all classes used in training
+contry_of_res = st.selectbox("Country of Residence", ['United States', 'Kenya', 'Others'])  # Add actual options from your training data
+result = st.slider("AQ-10 Result Score", 0, 10)
+relation = st.selectbox("Who is completing the test?", ['Parent', 'Self', 'Others'])
+
+# Mappings
 gender_map = {'m': 1, 'f': 0}
 binary_map = {'yes': 1, 'no': 0}
+ethnicity_map = {'White-European': 0, 'Latino': 1, 'Others': 2}
+country_map = {'United States': 0, 'Kenya': 1, 'Others': 2}
+relation_map = {'Parent': 0, 'Self': 1, 'Others': 2}
 
-# Create DataFrame from inputs
+# Create input DataFrame
 input_data = pd.DataFrame([[
     A1, A2, A3, A4, A5, A6, A7, A8, A9, A10,
     age,
     gender_map[gender],
+    ethnicity_map[ethnicity],
     binary_map[jaundice],
     binary_map[austim],
-    binary_map[used_app_before]
-]])
-
+    country_map[contry_of_res],
+    binary_map[used_app_before],
+    result,
+    relation_map[relation]
+]], columns=[
+    'A1_Score', 'A2_Score', 'A3_Score', 'A4_Score', 'A5_Score', 'A6_Score', 'A7_Score', 'A8_Score', 'A9_Score', 'A10_Score',
+    'age', 'gender', 'ethnicity', 'jaundice', 'austim', 'contry_of_res', 'used_app_before', 'result', 'relation'
+])
 # Prediction
 if st.button("Predict"):
     prediction = model.predict(input_data)
